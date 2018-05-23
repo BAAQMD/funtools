21eacd2f2a6e2f21ef6508903cda9f0b89c64b20
David Holstius <david.holstius@gmail.com>
2018-05-23 15:54:12 -0700

    tidy documentation

--- R/md5.R
+++ R/md5.R
@@ -2,8 +2,10 @@
 #'
 #' @param x any R object
 #' @param \dots further arguments to \code{digest::digest}
+#'
 #' @return MD5 digest (a.k.a. hash) of \code{x}
 #' @importFrom digest digest
+#'
 #' @export
 md5 <- function (x, ...) {
   digest::digest(x, algo = "md5", ...)
