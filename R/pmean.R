757ba473f72ad0b0e95956971feb861f459b9c34
David Holstius <david.holstius@gmail.com>
2022-06-24 18:08:13 -0700

    Update pmean.R

--- R/pmean.R
+++ R/pmean.R
@@ -17,3 +17,25 @@ pmean <- function (..., na.rm = TRUE) {
   f <- function (...) base::mean(c(...), na.rm = na.rm)
   purrr::pmap_dbl(args, f)
 }
+
+#' papply
+#'
+#' Compute a function across vectors (similar to `pmax` or `pmin`).
+#' Lazy evaluation meas this will work inside a `dplyr::mutate()`.
+#'
+#' @param ... one or more vectors
+#' @param .fun (function) must return a single numeric result
+#' @param na.rm passed to `.fun`
+#'
+#' @return double precision
+#'
+#' @importFrom lazyeval lazy_dots lazy_eval
+#' @importFrom purrr pmap_dbl
+#'
+#' @export
+papply <- function (..., .fun, na.rm = TRUE) {
+  dots <- lazyeval::lazy_dots(...)
+  args <- lazyeval::lazy_eval(dots)
+  f <- function (...) .fun(c(...), na.rm = na.rm)
+  purrr::pmap_dbl(args, f)
+}
